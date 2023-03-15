f5d7301b05be20de0945897c88dbea40c1f54491
zaicon001 <afeez.adebayo11@gmail.com>
2023-03-15 13:08:51 +0100

    breakout session

--- Arrayclass.js
+++ Arrayclass.js
@@ -191,20 +191,20 @@ let ans =  nums[nums[index]]
 
  //Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 
-Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+//Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 
  
 
-Example 1:
+//Example 1:
 
-Input: nums = [2,5,1,3,4,7], n = 3
-Output: [2,3,5,4,1,7] 
-Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
-Example 2:
+//Input: nums = [2,5,1,3,4,7], n = 3
+//Output: [2,3,5,4,1,7] 
+//Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+//Example 2:
 
-Input: nums = [1,2,3,4,4,3,2,1], n = 4
-Output: [1,4,2,3,3,2,4,1]
-Example 3:
+//Input: nums = [1,2,3,4,4,3,2,1], n = 4
+//Output: [1,4,2,3,3,2,4,1]
+//Example 3:
 
 Input: nums = [1,1,2,2], n = 2
 Output: [1,2,1,2]
@@ -224,3 +224,62 @@ let yOneToN = nums[xOneToN - 1]
 nums.forEach(function(number,index){
  return 
 })
+
+
+    //BREAKOUT SESSION ......
+
+    // FOR EACH 
+
+    let arr = [10,20,30,40,50]
+    arr[0] + 10
+    arr[1] + 10
+    arr[2] + 10
+    arr[3] + 10
+    arr[4] + 10
+  
+    arr.forEach(function saeed(){
+      //logic
+      return
+    }) // forEach method. A callback is a function that is passed as a parameter
+
+    arr.forEach(function (){
+      //logic
+      return
+    }) // A function without a function name is called ANONYMOUS FUNCTION 
+
+    // forEach expect two parameters to be passed inside its function. 
+
+    arr.forEach(function(item, index){
+      return arr + 10 
+    })
+
+    //Write a function that brings all the items, and index in a bracket. i.e. (item[0], index), (item[1], index) etc. 
+
+    let arrRes = []
+    arr.forEach(function(item, index){
+      //arrRes.push(`(${item}, ${index})`)
+      arrRes.push("(" + item + "," + index + ")")
+    })
+    console.log(arrRes)
+
+    // It'll return ['(10,0)','(20,1)','(30,2)', '(40,3)', '(50,4)']
+
+    let array = [10,20,30,40,50]
+    let results = []
+    arr.forEach(function(item, index){
+      results.push(item * 5)
+    })
+    console.log(results)
+
+// SORT 
+//SORT WITH NOT PARAMETERS 
+let numb = ["zain", "ayo", "ade","tola", "tolami", "chika"]
+console.log(numb.sort()) // ["ade", "ayo", "tola", "tolami", "chika", "zain"]
+
+
+let arr3 = [34,30,1,130,10,20,301,40,50]
+console.log(arr3.sort()) // [1,10,130,20,30,301,34,40,50] According to ASCII
+
+
+
+
